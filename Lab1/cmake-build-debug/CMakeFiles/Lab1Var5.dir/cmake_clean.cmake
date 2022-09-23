ea12fe46cc4cc0fa972ca580708753d1cd83f537
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2022-09-23 20:20:59 +0300

    Started Lab1, implemented base logic

---
+++ Lab1/cmake-build-debug/CMakeFiles/Lab1Var5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab1Var5.dir/main.c.o"
+  "Lab1Var5"
+  "Lab1Var5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab1Var5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab1/cmake-build-debug/CMakeFiles/Lab1Var5.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Lab1Var5.
+# This may be replaced when dependencies are built.
